feec6a8edae3122a10511e3c57d7ab707e31fb38
discipvlvsaeternvs <discipvlvs@gmail.com>
2023-06-20 00:11:06 +0700

    Initial commit

---
+++ register.php
@@ -0,0 +1,31 @@
+<?php
+include("database.php");
+
+$e = $_POST['email'];
+$n = $_POST['name'];
+$p = $_POST['password'];
+$t = $_POST['type'];
+
+$check_prior_registration_sql = "SELECT id FROM Users WHERE email = '$e'";
+$res = mysqli_query($conn, $check_prior_registration_sql);
+
+if ($res) {
+	if (mysqli_num_rows($res) > 0) {
+		header("Location: register-form.php?status=userExists");
+	} else {
+        try{
+            $register_sql = "INSERT INTO Users (email, name, password, type, register_datetime) VALUES ('$e', '{$n}', MD5('{$p}'), {$t}, NOW())";
+            $res = mysqli_query($conn, $register_sql);
+        } catch(mysqli_sql_exception) {
+            die("Error while inserting");
+        }
+
+		if ($res) {
+			header("Location: login-form.php?status=registSuccessful");
+		} else {
+			header("Location: register-form.php?status=registFailed");
+		}
+	}
+} else {
+    die("Error while fetching query");
+}
